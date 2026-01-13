ee46498feb34befba820a85fbe53ea773d64029d
Viktorplus <36795799+viktorplus@users.noreply.github.com>
2026-01-14 00:22:09 +0100

    add homework21

---
+++ homework/homework21.py
@@ -0,0 +1,44 @@
+"""Python Fundamentals 2025: Домашнее задание 21
+Повторения букв
+Реализуйте функцию, которая принимает текст и возвращает словарь с подсчётом количества каждой буквы, игнорируя регистр.
+Данные:
+text = "Programming is fun!"
+Пример вывода:
+{'p': 1, 'r': 2, 'o': 1, 'g': 2, 'a': 1, 'm': 2, 'i': 2, 'n': 2, 's': 1, 'f': 1, 'u': 1}"""
+
+# from collections import Counter
+# text = "Programming is fun!"
+#
+# def text_to_dict (text):
+#     return dict(Counter(text.replace('!', '').replace('.', '').replace(',', '').replace(' ', '').lower()))
+#
+# print(text_to_dict(text))
+
+# вариант 2
+# from collections import Counter
+#
+# def text_to_dict(text):
+#     letters = [c.lower() for c in text if c.isalpha()]
+#     return dict(Counter(letters))
+#
+# text = "Programming is fun!"
+# print(text_to_dict(text))
+
+
+"""Группировка студентов по классам
+Создайте структуру для группировки студентов по классам.
+Добавьте студентов в соответствующие группы.
+Данные:
+students = [("class1", "Alice"), ("class2", "Bob"), ("class1", "Charlie"), ("class3", "Daisy")]
+Пример вывода:
+{'class1': ['Alice', 'Charlie'], 'class2': ['Bob'], 'class3': ['Daisy']}"""
+
+from collections import defaultdict
+students = [("class1", "Alice"), ("class2", "Bob"), ("class1", "Charlie"), ("class3", "Daisy")]
+
+def group_by_class (students):
+    grupped = defaultdict(list)
+    for student in students:
+        grupped[student[0]].append(student[1])
+print(group_by_class(students))
+
